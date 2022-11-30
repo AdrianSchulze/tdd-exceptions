bbc65926b81ab6baebd91f99dfd9ffbe233407b0
AdrianSchulze <adrian@sei-sicherer.de>
2022-11-30 17:05:04 +0100

    Init: Jabadabadu

---
+++ src/test/java/de/neuefische/GuestListTest.java
@@ -0,0 +1,13 @@
+package de.neuefische;
+
+import org.junit.jupiter.api.Test;
+
+class GuestListTest {
+
+    @Test
+    void shouldBeEmptyInitially() {
+        GuestList guestlist = new GuestList();
+
+    }
+
+}
